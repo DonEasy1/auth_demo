9a22776ca473c0544193a11214ed133387efed9a
Dawn <doneasy1@gmail.com>
2019-07-03 03:33:23 +0000

    added register routes and register form

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 * Add secret route and template
 
 # Auth Demo Part 2
-* Create USer model
+* Create User model
 * Configure passport
 
-
+# Auth CodeAlong Part 3
+* Add Register routes
+* Add Register form
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var express = require('express'),
 mongoose.connect("mongodb://localhost:27017/auth_demo", { useNewUrlParser: true }); 
 var app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-session')({
 	secret: "Rusty is the best dog.",
 	resave: false,
@@ -21,14 +21,65 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get ('/', (req, res) => {
-	res.render('home');
+//===================
+//ROUTES
+//===================
+
+app.get("/", function(req, res){
+    res.render("home");
 });
 
-app.get("/secret", (req, res) => {
-	res.render("secret");
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+app.get("/secret", function(req, res){
+   res.render("secret"); 
+});
+
+//handling user sign up
+app.post("/register", (req, res) => {
+	// req.body.username
+	// req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) =>{
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, () => {
+           res.redirect("/secret");
+        });
+    });
 });
 
 app.listen(3000, () => {
-	console.log('listening');
+	console.log('server listening');
 });
+// LOGIN ROUTES
+//render login form
+// app.get("/login", function(req, res){
+//    res.render("login"); 
+// });
+//login logic
+//middleware
+// app.post("/login", passport.authenticate("local", {
+//     successRedirect: "/secret",
+//     failureRedirect: "/login"
+// }) ,function(req, res){
+// });
+
+// app.get("/logout", function(req, res){
+//     req.logout();
+//     res.redirect("/");
+// });
+
+
+// function isLoggedIn(req, res, next){
+//     if(req.isAuthenticated()){
+//         return next();
+//     }
+//     res.redirect("/login");
+// }
